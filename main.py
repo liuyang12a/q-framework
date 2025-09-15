1e05243093b66256a5849dec396b0cbccde990dc
Yang Liu <liuyang12a@nudt.edu.cn>
2025-09-15 09:59:40 +0800

    install tqdm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 
 from data_loader import load_network_from_csv, digraph_to_adjacency_matrix, save_to_json, load_json_file
 from q_model import SI,SIR,SIRS
@@ -26,20 +27,25 @@ if __name__ == "__main__":
     network = load_network_from_csv("data/network.csv")
     adj_mtx, nodes = digraph_to_adjacency_matrix(network)
 
-    sp = SI(nodes, adj_mtx, lam=0.5, alpha=1.0)
-    init_spreading = np.zeros(len(sp.states))
-    init_spreading[SPREADING_SOURCE] = 1.0
-    dynamics, distances = sp.run(init_spreading_state=init_spreading, max_time=MAX_TIME, cycle_time=CYCLE_TIME)
+    total_point = 100
+    with tqdm(total=total_point) as pbar:
+        for lam in np.linspace(0.1,0.9,100):
 
-    results = {
-        'model': sp.__class__.__name__,
-        'params': sp.params,
-        'start_state': init_spreading,
-        'stationary': sp.stationary,
-        'dynamics': dynamics,
-        'distances': distances
-    }
+            sp = SI(nodes, adj_mtx, lam=lam, alpha=1.0)
+            init_spreading = np.zeros(len(sp.states))
+            init_spreading[SPREADING_SOURCE] = 1.0
+            dynamics, distances = sp.run(init_spreading_state=init_spreading, max_time=MAX_TIME, cycle_time=CYCLE_TIME)
 
-    print(results)
+            results = {
+                'model': sp.__class__.__name__,
+                'params': sp.params,
+                'start_state': init_spreading,
+                'stationary': sp.stationary,
+                'dynamics': dynamics,
+                'distances': distances
+            }
+
+            save_to_json(results, "cache/%s-SI-lam=%.2f"%(get_today(),lam))
+            pbar.update(1)
 
 
